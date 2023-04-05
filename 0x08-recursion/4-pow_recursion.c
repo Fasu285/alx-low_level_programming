deb9dcd8a903e27b30a5f52eb19a0c9dcd2e977a
Fasu285 <fasu285@gmail.com>
2023-04-05 09:23:40 -0700

    hot fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,10 +2,9 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x: value of raise
- * @y: The power for x
- *
- * Returns: return the result x power y
+ * @x: value to raise
+ * @y: power of x
+ * Return: result of x power y
  */
 int _pow_recursion(int x, int y)
 {
