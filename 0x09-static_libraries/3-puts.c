92d10d969b90e10caa65dddd1c119131522e67a8
Fasu285 <fasu285@gmail.com>
2023-04-12 11:39:52 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* _puts -> function string parameter*/
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ * _puts -> function string parameter
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
